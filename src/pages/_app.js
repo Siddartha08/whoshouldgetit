123e486ccd885bad97da02cc52944c128343a06d
Siddartha08 <jemery89@gmail.com>
2021-01-29 15:48:14 -0600

    removed unused files

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -2,12 +2,8 @@ import React from "react";
 import "./../styles/global.scss";
 import NavbarCustom from "./../components/NavbarCustom";
 import IndexPage from "./index";
-import AboutPage from "./about";
-import FaqPage from "./faq";
-import PricingPage from "./pricing";
-import ContactPage from "./contact";
-import DashboardPage from "./dashboard";
-import SettingsPage from "./settings";
+
+
 import { Switch, Route, Router } from "./../util/router.js";
 import NotFoundPage from "./not-found.js";
 import Footer from "./../components/Footer";
@@ -31,18 +27,6 @@ function App(props) {
 
         <Switch>
           <Route exact path="/" component={IndexPage} />
-
-          <Route exact path="/about" component={AboutPage} />
-
-          <Route exact path="/faq" component={FaqPage} />
-
-          <Route exact path="/pricing" component={PricingPage} />
-
-          <Route exact path="/contact" component={ContactPage} />
-
-          <Route exact path="/dashboard" component={DashboardPage} />
-
-          <Route exact path="/settings/:section" component={SettingsPage} />
 
           <Route component={NotFoundPage} />
         </Switch>
